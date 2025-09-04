b434a25ee769d06a6b83df24c1b8172f8feccd2b
dagm <dagimyibabe19@gmail.com>
2025-09-05 01:23:26 +0300

    Add workout details component and fetch workouts
    Introduced a new WorkoutDetails component to display individual workout information. Updated Home page to fetch workouts from the API and render them using the new component. Added related CSS styles for the homepage and workout details. Configured Vite dev server to proxy API requests to the backend.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,28 @@
+import { useEffect, useState } from "react";
+
+//components
+import WorkoutDetails from "../components/WorkoutDetails";
+
 export default function Home() {
+    const [workouts, setWorkouts] = useState(null);
+    useEffect(() => {
+        const fetchWorkouts = async () => {
+            const response = await fetch("/api/workouts/");
+            const data = await response.json();
+            if (response.ok) {
+                setWorkouts(data)
+            }
+        };
+        fetchWorkouts();
+    }, []);
+
     return (
         <div className="home">
-            <h2>Home</h2>
+            <div className="workouts">
+                {workouts && workouts.map((workout)=>(
+                    <WorkoutDetails key={workout._id} workout={workout}/>
+                ))}
+            </div>
         </div>
     );
 }
