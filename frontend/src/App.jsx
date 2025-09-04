6851f565004ff0319eaa126c6933a81e01d07064
dagm <dagimyibabe19@gmail.com>
2025-09-05 00:51:21 +0300

    Add Navbar component and basic layout styling
    Introduced a new Navbar component and integrated it into App.jsx for consistent navigation. Added initial CSS for layout and styling, including font imports and container styles.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,22 +2,21 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 //pages and components
 import Home from "./pages/Home";
+import Navbar from "./components/navbar";
 
 function App() {
-  return(
-    <div className="App">
-        <BrowserRouter>
-            <div className="pages">
-                <Routes>
-                    <Route 
-                    path="/"
-                    element={<Home/>}
-                    />
-                </Routes>
-            </div>
-        </BrowserRouter>
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Navbar/>
+                <div className="pages">
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                    </Routes>
+                </div>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
